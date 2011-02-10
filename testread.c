62c480a450f14a722e906defafa64084ccfd8a04
Jason A. Donenfeld <Jason@zx2c4.com>
2011-02-09 22:24:25 -0500

    Dump all the info in the struct, seperated by tab.

--- testread.c
+++ testread.c
@@ -5,7 +5,16 @@ int main()
 {
 	struct utmp entry;
 	while (fread(&entry, sizeof(struct utmp), 1, stdin)) {
-		printf("%s\n", entry.ut_host);
+		#if __WORDSIZE == 64 && defined __WORDSIZE_COMPAT32
+		char *formatString = "%hd\t%d\t%s\t%s\t%s\t%s\t%hd\t%hd\t%d\t%d\t%d\t%s\n";
+		#else
+		char *formatString = "%hd\t%d\t%s\t%s\t%s\t%s\t%hd\t%hd\t%ld\t%ld\t%ld\t%s\n"
+		#endif
+		printf(formatString, entry.ut_type, entry.ut_pid, entry.ut_line,
+				     entry.ut_id, entry.ut_user, entry.ut_host,
+				     entry.ut_exit.e_termination, entry.ut_exit.e_exit,
+				     entry.ut_session, entry.ut_tv.tv_sec,
+				     entry.ut_tv.tv_usec, "IPADDRESS");
 	}
 	return 0;
 }
