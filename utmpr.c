f649f05ae48f7cbcf2d87226c865bbb9271e4c8f
Jason A. Donenfeld <Jason@zx2c4.com>
2011-02-09 22:46:14 -0500

    Print IP addresses and do proper formatting for int32_t.

--- utmpr.c
+++ utmpr.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include <utmp.h>
 
 int main()
 {
 	struct utmp entry;
 	while (fread(&entry, sizeof(struct utmp), 1, stdin)) {
+		char ip[16];
+		if (0) //TODO: determine how to figure out of it's an ipv6. entries 1 2 and 3 are zero perhaps?
+			ip[0] = 'a'; //TODO: format ipv6 address
+		else
+			sprintf(ip, "%d.%d.%d.%d",
+				(entry.ut_addr_v6[0] >> 0x0) & 0xFF,
+				(entry.ut_addr_v6[0] >> 0x8) & 0xFF,
+				(entry.ut_addr_v6[0] >> 0x10) & 0xFF,
+				(entry.ut_addr_v6[0] >> 0x18) & 0xFF);
+
 		#if __WORDSIZE == 64 && defined __WORDSIZE_COMPAT32
-		char *formatString = "%hd\t%d\t%s\t%s\t%s\t%s\t%hd\t%hd\t%d\t%d\t%d\t%s\n";
+		char *formatString = "%hd\t%d\t%s\t%s\t%s\t%s\t%hd\t%hd\t%"PRId32"\t%"PRId32"\t%"PRId32"\t%s\n";
 		#else
 		char *formatString = "%hd\t%d\t%s\t%s\t%s\t%s\t%hd\t%hd\t%ld\t%ld\t%ld\t%s\n"
 		#endif
@@ -14,7 +25,7 @@ int main()
 				     entry.ut_id, entry.ut_user, entry.ut_host,
 				     entry.ut_exit.e_termination, entry.ut_exit.e_exit,
 				     entry.ut_session, entry.ut_tv.tv_sec,
-				     entry.ut_tv.tv_usec, "IPADDRESS");
+				     entry.ut_tv.tv_usec, ip);		
 	}
 	return 0;
 }
